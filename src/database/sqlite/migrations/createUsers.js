7e5150519cd830de6c8f1739f24e0b9bccf2ba76
Isa <bela.tina@hotmail.com>
2024-08-05 22:28:08 -0300

    first commit

---
+++ src/database/sqlite/migrations/createUsers.js
@@ -0,0 +1,14 @@
+//thie is a migration without knex.js. All in SQL language pure.
+const createUsers = `
+    CREATE TABLE IF NOT EXISTS users (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR,
+    email VARCHAR,
+    password VARCHAR,
+    avatar VARCHAR NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+`;
+
+module.exports = createUsers;
